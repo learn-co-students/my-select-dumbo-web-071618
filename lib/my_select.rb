e73300790d0887c0d0662851dc86ef813c2009e3
wachka06 <wachka06@gmail.com>
2018-07-12 15:42:40 +0000

    Done.

--- lib/my_select.rb
+++ lib/my_select.rb
@@ -1,3 +1,17 @@
 def my_select(collection)
  # your code here!
+ 
+ new_array = []
+ i = 0
+ 
+ while i < collection.length 
+  if yield(collection[i]) == true
+    new_array << collection[i]
+  end
+ i += 1 
+ end
+ 
+ collection
 end
+
+
